97c54ef6b0921f23e9c0d1833f68dd4b4b1572e5
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2026-04-16 20:04:15 +0900

    typo

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     minify: 'terser',
-    sourceMap: true,
+    sourcemap: true,
     terserOptions: {
       compress: {
         drop_console: true,
